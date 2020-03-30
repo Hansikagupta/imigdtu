229deed89a5db94e31d997535d6f3d248ec3334e
hansika <hansikakgupta@gmail.com>
2020-03-30 23:07:46 +0530

    First Commit
    Uploading project without graphs of surveys.

---
+++ minorProject/footer.php
@@ -0,0 +1,4 @@
+<footer class="Footer bg-dark dker">
+    <p>imigdtu
+        <br>All Rights Reserved Copyright 2019</p>
+</footer>
---
+++ minorProject/modal.php
@@ -0,0 +1,244 @@
+
+<!-- Modal For University Objective-->
+<div id="objective" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h3 class="modal-title">University Objective</h3>
+      </div>
+      <div class="modal-body">
+
+        <ol class="lead">
+          <li>Apply principles particularly in the solution of problems related to information technology and its allied field developments. </li>
+          <li>Conduct relevant researches and development activities in the field of information technology and its allied fields. </li>
+          <li>Promote the development and transfer of appropriate information technology and its allied fields</li>
+          <li>Manage information technology and its allied fields protects and enterprise that promote expertise. </li>
+          <li>Produce morally, and upright professionals with the entry level competency.</li>
+        </ol>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+
+<!-- Modal For University Objective-->
+<div id="goal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h3 class="modal-title">University Goal</h3>
+      </div>
+      <div class="modal-body">
+
+        <p class="lead text-center">
+          Excellence, Competence and Educational Leadership in Science and Technology
+        </p>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+
+<!-- Modal For Over view-->
+<div id="Overview" class="modal fade" role="dialog">
+  <div class="modal-dialog" style="width: 900px;">
+
+    <!-- Modal content-->
+    <div class="modal-content" >
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Overview</h4>
+      </div>
+      <div class="modal-body">
+        <h2 class="lead" style=" text-indent: 50px;"><strong> Indira Gandhi Delhi Technical University for Women    </strong>
+</h2>
+ <p class="lead" style=" text-indent: 50px;">
+Indira Gandhi Delhi Technical University for Women (IGDTUW) has been upgraded from Indira Gandhi Institute of Technology in May 2013 vide Delhi State Legislature Act 9, 2012, as a non-affiliating teaching and research University at Delhi to facilitate and promote studies, research, technology, innovation, incubation and extension work in emerging areas of professional education among women, with focus on engineering, technology, applied sciences, management and its allied areas with the objective to achieve excellence in these and related fields.
+</p>
+ <p class="lead" style=" text-indent: 50px;">
+Erstwhile Indira Gandhi Institute of Technology (IGIT) was established in 1998 by Directorate of Training and Technical Education, Govt of N.C.T of Delhi as the first engineering college for women only. From 2002- 2013, the college remained the first and only constituent college of Guru Gobind Singh Indraprastha University.
+</p>
+ <p class="lead" style=" text-indent: 50px;">
+The upgradation of IGIT, already a prestigious college of north India, to University has enabled it to function even more efficiently as a teaching and research centre in various branches of engineering, promoting advancement and dissemination of knowledge and learning among women of the country and to meet the requirement of technically skilled women workforce. The objective of the University is to foster industry relevant research and innovations  and empower the women of our country through value based higher education making them employable, self reliant, responsible citizen of the country with concern for environment and society.
+</p>
+ <!-- <p class="lead" style=" text-indent: 50px;">
+In <i>1998</i>, when the College was elevated to University status, the <strong> School of Engineering (SEng)</strong> was subsequently elevated to the College level and was named as the <strong>College of Engineering (CEng)</strong>.
+</p>
+ <p class="lead" style=" text-indent: 50px;">
+To accommodate IT courses offerings; the College was renamed as the <strong>College of Engineering and Information Technology (CEIT)</strong> in <i>2001</i>.
+</p>
+ <p class="lead" style=" text-indent: 50px;">
+ As approved by the Board of Regents, <strong>CEIT</strong> offers various engineering programs, computer-based programs, and industrial technology programs
+ </p> -->
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+<!-- Modal For System Developer-->
+<div id="SystemDeveloper" class="modal fade" role="dialog">
+  <div class="modal-dialog" style="width: 900px;">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">System Developer</h4>
+      </div>
+      <div class="modal-body">
+        <div class="row main-low-margin text-center">
+                <div class="col-md-3 ">
+                    <img class="img-circle" src="assets/img/creator/img1.jpg" alt="">
+                    <h4><strong>Hansika Gupta</strong> </h4>
+                    <p>
+                        Bachelor Of Technology In Computer Science ( BTech, CSE )<br> (IGDTUW)
+
+                    </p>
+                    <p>
+
+                    </p>
+
+                </div>
+                <h3>Introduction of Authors</h3>
+             <hr>
+                <div class="col-md-8 col-sm-8 text-justify">
+
+                    <p class="lead" style=" text-indent: 50px;">
+                        The author was born in dasmariñas cavite on January 27,1993. She is  the youngest  among the two children of Mr.Antonio D. Bulanhagui and Mrs. Shirley R. Bulanhagui.
+  She finished her primary education at the Dasmariñas II Central School(formerly DBBES-B) in 2005. She obtained her secondary education from DNHS-Main in 2009. In 2010, she then enrolled and graduate in Associate Degree of Computer Science(2 years) at the AISAT Dasmarinas. And decide to enrolled in the bachelor’s degree to continue and pursue the degree of Computer Science at the Cavite State University-Main in 2013 up to present.
+
+                    </p>
+                </div>
+
+            </div>
+            <div class="row main-low-margin text-center">
+
+              <hr>
+                <div class="col-md-8 col-sm-8 text-justify">
+
+                    <p class="lead" style=" text-indent: 50px;">
+                         The author was born in Naic Cavite on September 27, 1996. He is  the eldest  among the four children of Mr. Herman P. Villadolid and Mrs. Alpha N. Villadolid.
+  He finished her primary education at the Naic Elementary School  in 2009. She obtained her secondary education from Cavite Community Academy in 2013. And decide to enrolled in the bachelor’s degree of Computer Science at the Cavite State University-Main in 2013 up to present.
+
+                    </p>
+                </div>
+                <div class="col-md-3 ">
+                    <img class="img-circle" src="assets/img/creator/img2.jpg" alt="">
+                    <h4><strong>John Ervin N. Villadolid</strong> </h4>
+                    <p>
+                        Bachelor Of Science In Computer Science ( BSCS )<br> Cavite State University (CvSU)
+                    </p>
+                    <p>
+                       <a href="https://web.facebook.com/The.OtherProtagonist" target="_blank"><i class="fa fa-facebook-square fa-2x color-facebook"></i></a>
+                    </p>
+
+                </div>
+
+            </div>
+            <div class="row main-low-margin text-center">
+                <div class="col-md-3 ">
+                    <img class="img-circle" src="assets/img/creator/img3.jpg" alt="">
+                    <h4><strong>Ezra Marie F. Ramos</strong> </h4>
+                    <p>
+                        Professor(Thesis adviser)<br> Cavite State University (CvSU)
+
+                    </p>
+                    <p>
+                       <a href="" target="_blank"><i class="fa fa-facebook-square fa-2x color-facebook"></i></a>
+
+                    </p>
+
+                </div>
+             <hr>
+                <div class="col-md-8 col-sm-8 text-justify">
+
+                    <p class="lead" style=" text-indent: 50px;">
+                         She finished the bachelor’s degree of computer science at the cavite state university-main in april 2011, she then enrolled in masteral’s degree of computer science at the AMA University-Makati. She is currently the instructor of computer science student of department of information technology.
+
+                    </p>
+                </div>
+
+            </div>
+            <hr>
+            <div class="row main-low-margin text-center">
+
+
+                <div class="col-md-8 col-sm-8 text-justify">
+                    <p class="lead" style=" text-indent: 50px;">
+                        She finished the bachelor’s degree of computer science at the Cavite State University-Main in april 2010, she then enrolled in masteral’s degree of information technology at the Cavite State University- Main. She is currently the instructor of computer science student of department of information technology.
+                    </p>
+                </div>
+                <div class="col-md-3 ">
+                    <img class="img-circle" src="assets/img/creator/img4.jpg" alt="">
+                    <h4><strong> Lorielle S. Rocela</strong> </h4>
+                    <p>
+                        Professor(Technical Critic)<br> Cavite State University (CvSU)
+
+                    </p>
+                    <p>
+                       <a href="" target="_blank"><i class="fa fa-facebook-square fa-2x color-facebook"></i></a>
+
+                    </p>
+
+                </div>
+
+            </div>
+            <div class="row main-low-margin text-center">
+                <div class="col-md-3 ">
+                    <img class="img-circle" src="assets/img/creator/webcreator.jpg" alt="">
+                    <h4><strong>Rhalp Darren Cabrera</strong> </h4>
+                    <p>
+                        Bachelor Of Science In Information Technology<br>( BSIT )<br> Cavite State University (CvSU)
+
+                    </p>
+                    <p>
+                       <a href="https://www.facebook.com/rhalp10" target="_blank"><i class="fa fa-facebook-square fa-2x color-facebook"></i></a>
+                        <a href="https://mobile.twitter.com/rhalp10" target="_blank"><i class="fa fa-twitter-square fa-2x color-twitter"></i></a>
+                        <a href="https://github.com/rhalp10" target="_blank"><i class="fa fa-github-square fa-2x color-github"></i></a>
+
+
+                    </p>
+
+                </div>
+             <hr>
+                <div class="col-md-8 col-sm-8 text-justify">
+
+                    <p class="lead" style=" text-indent: 50px;">
+                         I’m Taking Up Bachelor of Science in Information Technology (BSIT), Like to Waste Time on The Front of Computer Making Random Things Like Programming I Know (C++, NetBeans Java, HTML, CSS, PHP, JSCRIPT, Visual Basic, Assembly, Storyboard Scripting), About on Arts and Music? I Used the Following Tools (Paint, Paint Tool Sai, Photoshop CC) And Used Them to Make Digital Art, Background, Cover, Editing Images and Also Edit Video, Sounds/Remix but Literally Not EXPERT More than Of My Skill Learned by My Self Study, My Psychology Friends say “Jack-of-all-trades, Master Of None”.
+
+                    </p>
+                </div>
+
+            </div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
