52bd099afe5d467f1ae5bc1a824f4b5a6102e211
hansika <hansikakgupta@gmail.com>
2020-05-04 15:15:26 +0530

    Update session.php

--- minorProject/session.php
+++ minorProject/session.php
@@ -1,17 +1,10 @@
-<?php
-// Establishing Connection with Server by passing server_name, user_id and password as a parameter
-
+<?php// Establishing Connection with Server by passing server_name, user_id and password as a parameter
 $url = parse_url("mysql://b997849ceafd11:d2115bcc@us-cdbr-east-06.cleardb.net/heroku_e7b0ec896a85723?reconnect=true");
-
 $server = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
-//
-
-$con = 
-new mysqli($server, $username, $password, $db)
- or die("ERROR");
+$con = new mysqli($server, $username, $password, $db) or die("ERROR");
 session_start(); // Starting Session
 // Storing Session
 $user_check = $_SESSION['login_user'];
