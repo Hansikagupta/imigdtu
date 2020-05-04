b3fd930fc8083b159bf3e416281237e1df7a28f2
hansika <hansikakgupta@gmail.com>
2020-05-04 17:59:24 +0530

    Error in session.php fixed

--- minorProject/session.php
+++ minorProject/session.php
@@ -1,4 +1,5 @@
-<?php// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+<?php
+// Establishing Connection with Server by passing server_name, user_id and password as a parameter
 $url = parse_url("mysql://b997849ceafd11:d2115bcc@us-cdbr-east-06.cleardb.net/heroku_e7b0ec896a85723?reconnect=true");
 $server = $url["host"];
 $username = $url["user"];
